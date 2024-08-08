ff9208ff9d7f9c52e185ec904bfb582271f96bf6
adeline MATHO <adelinematho0@gmail.com>
2024-08-08 17:32:16 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    'https://nkaffoadeline.github.io',
+   site: 'https://nkaffoadeline.github.io',
 })
